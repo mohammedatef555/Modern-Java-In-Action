988ef3bfda1a7d5195d5f7e3c107e05a064aac45
mohammedatef555 <42421703+mohammedatef555@users.noreply.github.com>
2022-08-22 14:06:21 +0400

    Finish Reading 5.6: Reducing

--- src/main/java/Chapter5/part3_Mapping/FlatteningStreams.java
+++ src/main/java/Chapter5/part3_Mapping/FlatteningStreams.java
@@ -8,20 +8,11 @@ import java.util.stream.Stream;
 public class FlatteningStreams {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("Hello", "world");
-//        String[] arr = {"Hello", "world"};
-//        Stream<String> stringStream = Arrays.stream(arr);
-//        stringStream.forEach(System.out::println);
-//        words.stream()
-//                .map(word -> word.split(""))
-//                .map(Arrays::stream)
-//                .forEach(System.out::println);
-        // Stream<String>
-        // Stream<String[]>
-        // Stream<Stream<String>>
-        words.stream()
+        List<String> l = words.stream()
                 .map(s -> s.split(""))
                 .flatMap(Arrays::stream)
                 .distinct()
-                .forEach(System.out::print);
+                .collect(Collectors.toList());
+        System.out.println(l); // [H, e, l, o, w, r, d]
     }
 }
